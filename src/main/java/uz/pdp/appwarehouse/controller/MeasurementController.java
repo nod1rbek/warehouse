8364439d7c253ba0420e90b2984beb0c38463df5
Sobirjon_Eshniyazov <sobirjoneshniyozov3858@gmail.com>
2022-03-05 14:23:08 +0300

    Measurement ustida CRUD amallari bajarildi

---
+++ src/main/java/uz/pdp/appwarehouse/controller/MeasurementController.java
@@ -0,0 +1,34 @@
+package uz.pdp.appwarehouse.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.appwarehouse.entity.Measurement;
+import uz.pdp.appwarehouse.payload.Result;
+import uz.pdp.appwarehouse.service.MeasurementService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/measurement")
+public class MeasurementController {
+
+    @Autowired
+    MeasurementService measurementService;
+
+    @PostMapping
+    public Result addMeasurementController(@RequestBody Measurement measurement) {
+        return measurementService.addMeasurementSercise(measurement);
+    }
+
+    @GetMapping
+    public List<Measurement> getMeasurementController() {
+        return measurementService.getMeasurements();
+    }
+
+    @GetMapping("/{id}")
+    public Measurement getMeasurementById(@PathVariable Integer id) {
+        return measurementService.getMeasurementById(id);
+    }
+
+
+}
